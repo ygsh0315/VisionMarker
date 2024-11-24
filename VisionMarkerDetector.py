34172efc078eabf54f6ff0b2f9e18d69572c9d98
Ellian <dbsrn0125@naver.com>
2024-11-24 16:33:58 +0900

    perspective 변환

---
+++ VisionMarkerDetector.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import VisionMarker as vm
+
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+vmd = vm.VisionMarker()
+
+while True:
+    ret, frame = cap.read()
+    lst, mask, contour = vmd.VisionDetect(frame)
+    marker_lst = vmd.CompareImg(lst)
+    
+    if not ret:
+        print("Can't open the Camera")
+        
+    cv2.imshow('marker', lst[0])
+    cv2.imshow('mask', mask)
+    cv2.imshow('Contour', contour)
+
+    print(marker_lst)
+
+    k = cv2.waitKey(1)
+    if k == 27:
+       break
+
+cap.release()
+cv2.destroyAllWindows()
--- visionMarker.py
+++ visionMarker.py
@@ -1,160 +1,177 @@
 import numpy as np
 import cv2
-import visionMarkerLibrary as vml
-
-#마커 배경 색상 지정 함수
-def SetMarker(key, bgr):
-    array = vml.vm_dict[key]
-
-    resized_arr = cv2.resize(array, (0, 0), fx=100, fy=100, interpolation=cv2.INTER_NEAREST)
-
-    # RGB 배열 생성
-    height, width = resized_arr.shape
-    bgr_array = np.ones((height, width, 3), dtype=np.uint8)
-
-    # 값에 따라 색상 지정
-    bgr_array[resized_arr == 0] = bgr  # 색상 지정
-    bgr_array[resized_arr == 1] = [255, 255, 255]
-
-    return bgr_array
-
-#마커 출력 함수
-def ShowMarker(key,bgr):
-    marker_i = SetMarker(key, bgr)
-    cv2.imshow("img",marker_i)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-# 인식한 마커와 dicionary에 저장된 마커 비교
-# def CompareImg(ImgPath):
-#     arr_dst = np.zeros((7,7), dtype=np.uint8)
-
-#     gray = cv2.imread(ImgPath, cv2.IMREAD_GRAYSCALE)
-#     _, bin = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
-#     h, w = bin.shape[:2]
-
-#     block_height = h // 7
-#     block_width = w // 7
-
-#     for y in range(7):  # 정확히 7번 반복
-#         for x in range(7):  # 정확히 7번 반복
-#             # 7x7 블록 슬라이싱
-#             start_y = y * block_height
-#             start_x = x * block_width
-#             block = bin[start_y:start_y + block_height, start_x:start_x + block_width]
-#             black_num = np.sum(block.flatten() == 0)
-#             white_num = np.sum(block.flatten() == 255)
-
-#             if black_num < white_num:
-#                 arr_dst[y, x] = 1
-
-#     for key, val in vml.vm_dict.items():
-#         if np.array_equal(val, arr_dst):
-#             return key
-#         else:
-#             print("No matching marker")
-        
-def CompareImg(lst):
-    marker_list = []
-    for img in lst:
-        cv2.imshow('w',img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        arr_dst = np.zeros((7,7), dtype=np.uint8)
-        gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        _, bin = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
-        h, w = bin.shape[:2]
-
-        block_height = h // 7
-        block_width = w // 7
-
-        for y in range(7):  # 정확히 7번 반복
-            for x in range(7):  # 정확히 7번 반복
-                # 7x7 블록 슬라이싱
-                start_y = y * block_height
-                start_x = x * block_width
-                block = bin[start_y:start_y + block_height, start_x:start_x + block_width]
-                black_num = np.sum(block.flatten() == 0)
-                white_num = np.sum(block.flatten() == 255)
-
-                if black_num < white_num:
-                    arr_dst[y, x] = 1
-        for key, val in vml.vm_dict.items():
-            if np.array_equal(val, arr_dst):
-                marker_list.append(key)
-        
-    return marker_list
-        # else:
-        #     print("No matching marker")
-
-def VisionDetect(img):  
-    #1. 영상을 불러온다.(이미지를 불러온다.)
-    received_img = img
-    #1.1 이미지 크기가 크니까 사이즈를 축소한다.
-    # received_img = cv2.resize(received_img, (0,0), fx=0.3, fy=0.3, interpolation=cv2.INTER_LINEAR)
-    #1.2 노이즈를 제거한다.
-    received_img_blur = cv2.GaussianBlur(received_img, (5, 5), 10)
-    # inRange 메서드를 사용하기 위해 hsv로 변환
-    img_hsv = cv2.cvtColor(received_img_blur, cv2.COLOR_BGR2HSV)
-    # marker 색상 범위에 해당하면 
-    mask = cv2.inRange(img_hsv, np.array([95, 215, 210], dtype=np.uint8), np.array([105, 220, 215], dtype=np.uint8))
-    res = cv2.bitwise_and(received_img, received_img, mask=mask)
-    res_gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-    _, res_bin = cv2.threshold(res_gray, 100, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(res_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(received_img, contours, -1, (0,0,255), 3)
-    cv2.imshow('test', received_img)
-    cv2.waitKey(10000)
-    cv2.destroyAllWindows
-    lst = []
-    for ct in contours:
-        x, y, w, h = cv2.boundingRect(ct)
-        lst.append(received_img[y:y+h, x:x+w])
-
-
-    # largest_contour = max(contours, key=cv2.contourArea)
-    # x, y, w, h = cv2.boundingRect(largest_contour)
-    # marker_img = received_img[y:y+h, x:x+w]
-
-
-    # cv2.imshow('Original Image', received_img)
-    # cv2.imshow('Detedcted Contour', mask)
-    # cv2.imshow('Largest Contour', marker_img)
-    # cv2.waitKey(10000)
-    # cv2.destroyAllWindows()
-
-    # cv2.drawContours(received_img, contours, -1, (0, 0, 255), 3)
-    # cv2.imshow('test', received_img)
-    # cv2.waitKey(10000)
-    # cv2.destroyAllWindows()
-
-
-
-    #3. ROI를 정사각형 모양으로 보정한다.
-    #4. 보정한 정사각형 모양의 ROI에 대해 CompareImg를 수행한다.        
-
-    # return marker_img
-    return lst
-                                                                             
-
-# HSV : [100 219 213]
-# ShowMarker("W",(213,152,30)) 
-# id = CompareImg("MarkerSamples/K.png")
-# print(id)
-img = cv2.imread("MarkerSamples/NoiseZ.png")
-marker_img = VisionDetect(img)
-# cv2.imshow('detected marker', marker_img)
-id = CompareImg(marker_img)
-print(id)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
+import VisionMarkerLibrary as vml
 
 class VisionMarker:
-    a = "value"
-    def __init__(self,attribute1):
-        self.a=attribute1
-        #동영상촬영, VisionDetect()
+    #마커 배경 색상 지정 함수
+    def SetMarker(self, key, bgr):
+        array = vml.vm_dict[key]
 
-#vm = VisionMarker()
+        resized_arr = cv2.resize(array, (0, 0), fx=100, fy=100, interpolation=cv2.INTER_NEAREST)
+
+        # RGB 배열 생성
+        height, width = resized_arr.shape
+        bgr_array = np.ones((height, width, 3), dtype=np.uint8)
+
+        # 값에 따라 색상 지정
+        bgr_array[resized_arr == 0] = bgr  # 색상 지정
+        bgr_array[resized_arr == 1] = [255, 255, 255]
+
+        return bgr_array
+
+    #마커 출력 함수
+    def ShowMarker(self, key, bgr):
+        marker_i = self.SetMarker(key, bgr)
+        cv2.imshow("img",marker_i)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def SaveMarker(self,key, bgr, path):
+        marker= self.SetMarker(key, bgr)
+        self.ShowMarker(key, bgr)
+        cv2.imwrite(path, marker)
+                
+    def CompareImg(self, lst):
+        marker_list = []
+        for img in lst:
+            arr_dst = np.zeros((7,7), dtype=np.uint8)
+            gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            _, bin = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
+            h, w = bin.shape[:2]
+
+            block_height = h // 7
+            block_width = w // 7
+
+            for y in range(7):  # 정확히 7번 반복
+                for x in range(7):  # 정확히 7번 반복
+                    # 7x7 블록 슬라이싱
+                    start_y = y * block_height
+                    start_x = x * block_width
+                    block = bin[start_y:start_y + block_height, start_x:start_x + block_width]
+                    black_num = np.sum(block.flatten() == 0)
+                    white_num = np.sum(block.flatten() == 255)
+                    if black_num < white_num:
+                        arr_dst[y, x] = 1
+            for key, val in vml.vm_dict.items():
+                if np.array_equal(val, arr_dst):
+                    marker_list.append(key)
+            
+        return marker_list
+
+    # def hsv_distance(self, pixel_hsv, target_hsv):
+    #     return np.linalg.norm(pixel_hsv - target_hsv)
+
+
+    # def testVisionDetect(self, img):  
+    #     #1. 영상을 불러온다.(이미지를 불러온다.)
+    #     received_img = img
+    #     #1.1 이미지 크기가 크니까 사이즈를 축소한다.
+    #     # received_img = cv2.resize(received_img, (0,0), fx=0.3, fy=0.3, interpolation=cv2.INTER_LINEAR)
+    #     #1.2 노이즈를 제거한다.
+    #     # received_img_blur = cv2.GaussianBlur(received_img, (5, 5), 10)
+    #     # inRange 메서드를 사용하기 위해 hsv로 변환
+    #     img_hsv = cv2.cvtColor(received_img, cv2.COLOR_BGR2HSV)
+
+    #     target_hsv = np.array([100, 80, 200], dtype=np.float32)
+
+    #     closest_hsv = None
+    #     min_distance = float('inf')
+
+    #     for y in range(img_hsv.shape[0]):
+    #         for x in range(img_hsv.shape[1]):
+    #             pixel_hsv = img_hsv[y, x].astype(np.float32)
+    #             distance = self.hsv_distance(pixel_hsv, target_hsv)
+    #             if distance < min_distance:
+    #                 min_distance = distance
+    #                 closest_hsv = pixel_hsv
+        
+    #     res = closest_hsv
+
+    #     # marker 색상 범위에 해당하면 
+    #     mask = cv2.inRange(img_hsv, res-10, res+10)
+    #     # res = cv2.bitwise_and(received_img, received_img, mask=mask)
+    #     # res_gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    #     # _, res_bin = cv2.threshold(res_gray, 100, 255, cv2.THRESH_BINARY)
+    #     # contours, _ = cv2.findContours(res_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #     # # cv2.drawContours(received_img, contours, -1, (0,0,255), 3)
+    #     # # cv2.imshow('test', received_img)
+    #     # # cv2.waitKey(10000)
+    #     # # cv2.destroyAllWindows
+    #     # lst = []
+    #     # for ct in contours:
+    #     #     x, y, w, h = cv2.boundingRect(ct)
+    #     #     lst.append(received_img[y:y+h, x:x+w])
+
+
+    #     #3. ROI를 정사각형 모양으로 보정한다.
+    #     #4. 보정한 정사각형 모양의 ROI에 대해, CompareImg를 수행한다.        
+
+    #     return mask
+
+    
+
+
+
+
+
+    def VisionDetect(self, img):  
+        #1. 영상을 불러온다.(이미지를 불러온다.)
+        received_img = img
+        #1.1 이미지 크기가 크니까 사이즈를 축소한다.
+        # received_img = cv2.resize(received_img, (0,0), fx=0.3, fy=0.3, interpolation=cv2.INTER_LINEAR)
+        #1.2 노이즈를 제거한다.
+        received_img_blur = cv2.GaussianBlur(received_img, (5, 5), 10)
+        # inRange 메서드를 사용하기 위해 hsv로 변환
+        img_hsv = cv2.cvtColor(received_img_blur, cv2.COLOR_BGR2HSV)
+        # marker 색상 범위에 해당하면
+        mask = cv2.inRange(img_hsv, np.array([50, 100, 100], dtype=np.uint8), np.array([150, 255, 255], dtype=np.uint8))
+        res = cv2.bitwise_and(received_img, received_img, mask=mask)
+        res_gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+        _, res_bin = cv2.threshold(res_gray, 100, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(res_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # cv2.drawContours(received_img, contours, -1, (0,0,255), 3)
+        # cv2.imshow('test', received_img)
+        # cv2.waitKey(10000)
+        # cv2.destroyAllWindows
+        lst = []
+        for ct in contours:
+            x, y, w, h = cv2.boundingRect(ct)
+
+            # 컨투어 근사화
+            epsilon = 0.02 * cv2.arcLength(ct, True)
+            approx = cv2.approxPolyDP(ct, epsilon, True)
+
+            # 꼭짓점이 4개인 경우만 처리
+            if len(approx) == 4:
+                # 좌표를 정렬하여 정확히 매핑
+                src_pts = approx.reshape(4, 2).astype(np.float32)
+
+                # 좌표를 각각 정렬: 좌표의 위치 기준으로 구분
+                sorted_pts = np.zeros((4, 2), dtype=np.float32)
+                s = src_pts.sum(axis=1)  # x + y
+                diff = np.diff(src_pts, axis=1)  # y - x
+
+                sorted_pts[0] = src_pts[np.argmin(s)]  # 왼쪽 위
+                sorted_pts[2] = src_pts[np.argmax(s)]  # 오른쪽 아래
+                sorted_pts[1] = src_pts[np.argmin(diff)]  # 오른쪽 위
+                sorted_pts[3] = src_pts[np.argmax(diff)]  # 왼쪽 아래
+
+                # 정사각형의 목적지 좌표
+                dst_pts = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype=np.float32)
+
+                # 원근 변환 매트릭스 계산
+                M = cv2.getPerspectiveTransform(sorted_pts, dst_pts)
+
+                # ROI를 원근 변환
+                dst = cv2.warpPerspective(received_img, M, (w, h))
+                lst.append(dst)
+
+        # 처리된 ROI 목록과 마스크, 결과 이미지 반환
+        return lst, mask, res
+
+
+        #3. ROI를 정사각형 모양으로 보정한다.
+        #4. 보정한 정사각형 모양의 ROI에 대해, CompareImg를 수행한다.        
+
+        return lst, mask, res
+                                                                                
